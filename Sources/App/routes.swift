41ec9706454f1204a3a80d81a16db35fad281a33
Dhritiman Saha <dhritiman4th@gmail.com>
2024-01-16 08:43:13 +0530

    Update routes.swift
    Update routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,7 +2,7 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req async in
-        "It works!"
+        "It works baby!"
     }
 
     app.get("hello") { req async -> String in
